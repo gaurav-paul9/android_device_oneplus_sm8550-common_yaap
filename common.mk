e3868de76e32cba52ede5991bcebf7afd643e124
gaurav-paul9 <paulgaurav9@gmail.com>
2026-09-11 09:48:05 +0530

    sm8550-common: Fix audio_policy_configuration.xml copy destination
    Match upstream rework: the vendor specific config is meant to land at
    /vendor/etc/audio_policy_configuration.xml (AOSP default path), not the
    audio/ subdir. Also normalize EOF newline in audio_effects.xml for
    parity with the dolby blob.

--- common.mk
+++ common.mk
@@ -71,7 +71,7 @@ PRODUCT_COPY_FILES += \
     $(CONFIG_HAL_SRC_DIR)/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
     $(CONFIG_PAL_SRC_DIR)/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
